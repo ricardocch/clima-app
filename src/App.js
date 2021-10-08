f1292a50a3f42e9160c90e63c7b0027a7732fd10
Wenceslaorojas <wenceslao.rojar@gmail.com>
2021-10-08 19:38:34 -0300

    PRUEBA

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">HOLA</div>;
+  return <div className="App">CHAU</div>;
 }
 
 export default App;
